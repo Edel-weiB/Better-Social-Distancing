66284b47dca05e80c84b12ad1e5932a595fe970e
jianwei <huang_jianwei1@hotmail.com>
2020-08-21 16:41:50 +0800

    Update Scan QR Page

--- ClientApplication/app/src/main/java/com/example/cotrace/ui/scan/ScanFragment.java
+++ ClientApplication/app/src/main/java/com/example/cotrace/ui/scan/ScanFragment.java
@@ -1,6 +1,7 @@
 package com.example.cotrace.ui.scan;
 
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.cotrace.R;
@@ -19,18 +21,22 @@ import com.example.cotrace.ui.settings.SettingsViewModel;
 
 public class ScanFragment extends Fragment {
 
-    private SettingsViewModel scanViewModel;
+    private ScanViewModel scanViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         scanViewModel =
-                ViewModelProviders.of(this).get(SettingsViewModel.class);
+                ViewModelProviders.of(this).get(ScanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
-        final TextView textView = root.findViewById(R.id.text_scan);
+        final ImageView blackBoxView = root.findViewById(R.id.black_box);
+
+//        final TextView textView = root.findViewById(R.id.text_scan);
         scanViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//                textView.setText(s);
+                blackBoxView.setImageResource(R.drawable.blackbox);
             }
         });
         return root;
