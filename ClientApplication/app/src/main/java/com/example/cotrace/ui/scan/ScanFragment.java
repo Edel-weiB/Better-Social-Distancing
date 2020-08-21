52ee1de31f8eb651e197e75df1dcfc096421c815
Hoyjoy <44149666+Hoyjoy@users.noreply.github.com>
2020-08-21 21:21:24 +0800

    QR code
    done partially qr code reader

--- ClientApplication/app/src/main/java/com/example/cotrace/ui/scan/ScanFragment.java
+++ ClientApplication/app/src/main/java/com/example/cotrace/ui/scan/ScanFragment.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,9 +14,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.cotrace.QRreader;
 import com.example.cotrace.R;
 import com.example.cotrace.ui.settings.SettingsViewModel;
 
@@ -29,16 +32,17 @@ public class ScanFragment extends Fragment {
         scanViewModel =
                 ViewModelProviders.of(this).get(ScanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
-        final ImageView blackBoxView = root.findViewById(R.id.black_box);
+//        final ImageView blackBoxView = root.findViewById(R.id.black_box);
 
-//        final TextView textView = root.findViewById(R.id.text_scan);
-        scanViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        Button qr_button = root.findViewById(R.id.qr_button);
+        qr_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-                blackBoxView.setImageResource(R.drawable.blackbox);
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), QRreader.class));
             }
         });
+
+
         return root;
     }
 
