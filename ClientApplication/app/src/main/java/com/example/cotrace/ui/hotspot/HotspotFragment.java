cf2fcf741cc8568519ef662333e08eaff10c84cb
jianwei <huang_jianwei1@hotmail.com>
2020-09-05 17:10:06 +0800

    heatmap done

--- ClientApplication/app/src/main/java/com/example/cotrace/ui/hotspot/HotspotFragment.java
+++ ClientApplication/app/src/main/java/com/example/cotrace/ui/hotspot/HotspotFragment.java
@@ -2,11 +2,14 @@ package com.example.cotrace.ui.hotspot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,30 +20,135 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.cotrace.MapsActivity;
 import com.example.cotrace.QRreader;
 import com.example.cotrace.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
-public class HotspotFragment extends Fragment {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private HotspotViewModel hotspotViewModel;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static android.content.ContentValues.TAG;
+
+public class HotspotFragment extends Fragment implements OnMapReadyCallback {
+
+//    private HotspotViewModel hotspotViewModel;
+    private View root;
+    GoogleMap mHeatMap;
+    MapView mMapView;
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        hotspotViewModel =
-                ViewModelProviders.of(this).get(HotspotViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_hotspots, container, false);
-        final TextView textView = root.findViewById(R.id.text_hotspots);
-        hotspotViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        Button mapButton = root.findViewById(R.id.mapBtn);
-        mapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getContext(), MapsActivity.class));
-            }
-        });
+//        hotspotViewModel =
+//                ViewModelProviders.of(this).get(HotspotViewModel.class);
+        try {
+            root = inflater.inflate(R.layout.fragment_hotspots, container, false);
+            MapsInitializer.initialize(this.getActivity());
+            mMapView = (MapView) root.findViewById(R.id.heatmap);
+            mMapView.onCreate(savedInstanceState);
+            mMapView.getMapAsync(this);
+        }
+        catch (android.view.InflateException e){
+            Log.e(TAG, "Hotspot Inflate exception");
+        }
+//        final TextView textView = root.findViewById(R.id.text_hotspots);
+//        hotspotViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mHeatMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mHeatMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mHeatMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        addHeatMap();
+    }
+
+    private void addHeatMap() {
+        List<LatLng> heatMapList = null;
+
+        // Get the data: latitude/longitude positions of place locations.
+        try {
+            heatMapList = readItems(R.raw.place_locations);
+        } catch (JSONException e) {
+            Toast.makeText(getContext(), "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+        }
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(heatMapList)
+                .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay = mHeatMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+
+    }
+
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
     }
 }
